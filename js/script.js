9a121e9c0f66a1daa89d1246e22631fb1ae28454
djeenwolfy <volkovpavelvladislavovich@gmail.com>
2018-10-15 16:20:37 +0300

    1 commit

---
+++ js/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+$(".pc1").click(function(){
+	$(".infoPc1").toggle(500);
+	});});
+
+$(document).ready(function(){
+$(".pc2").click(function(){
+	$(".infoPc2").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc3").click(function(){
+	$(".infoPc3").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc4").click(function(){
+	$(".infoPc4").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc5").click(function(){
+	$(".infoPc5").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc6").click(function(){
+	$(".infoPc6").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc7").click(function(){
+	$(".infoPc7").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc8").click(function(){
+	$(".infoPc8").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc9").click(function(){
+	$(".infoPc9").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc10").click(function(){
+	$(".infoPc10").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc11").click(function(){
+	$(".infoPc11").toggle(500);
+});});
+
+$(document).ready(function(){
+	$(".pc12").click(function(){
+	$(".infoPc12").toggle(500);
+});
+});
+// 	$(".infoPc1").animate({marginLeft:"302px"},500);
+//       $(".infoPc1").animate({marginTop:"252px"},500);
+// });
