1efce74aa5cb69274dcefc558c8124e7be2a2367
djeenwolfy <volkovpavelvladislavovich@gmail.com>
2018-10-18 17:40:13 +0300

    18 10 2018

--- js/script.js
+++ js/script.js
@@ -1,63 +1,138 @@
-$(document).ready(function(){
-$(".pc1").click(function(){
-	$(".infoPc1").toggle(500);
-	});});
+// $(document).ready(function(){
+//     $(".pc1").click(function(){
+//     $(".infoPc1").slideDown(1000);    
+//     $(".pc1").removeClass("pc1"); 
 
-$(document).ready(function(){
-$(".pc2").click(function(){
-	$(".infoPc2").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc3").click(function(){
-	$(".infoPc3").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc4").click(function(){
-	$(".infoPc4").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc5").click(function(){
-	$(".infoPc5").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc6").click(function(){
-	$(".infoPc6").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc7").click(function(){
-	$(".infoPc7").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc8").click(function(){
-	$(".infoPc8").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc9").click(function(){
-	$(".infoPc9").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc10").click(function(){
-	$(".infoPc10").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc11").click(function(){
-	$(".infoPc11").toggle(500);
-});});
-
-$(document).ready(function(){
-	$(".pc12").click(function(){
-	$(".infoPc12").toggle(500);
-});
-});
-// 	$(".infoPc1").animate({marginLeft:"302px"},500);
-//       $(".infoPc1").animate({marginTop:"252px"},500);
+    
 // });
+// });
+
+
+
+// $(document).ready(function(){
+ 
+
+
+//     $(".infoPc1").click(function(){
+//     $(".infoPc1").slideUp(1000);
+
+// });
+// });
+
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc1").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc1").slideDown(1000); 
+	 } else {
+	 	$(".infoPc1").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked2 = false;
+	 $(".pc2").click(function(){
+	 	clicked2 = !clicked2;
+	 	//console.log(clicked);
+	 	if(clicked2) {
+	 	$(".infoPc2").slideDown(1000); 
+	 } else {
+	 	$(".infoPc2").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked3 = false;
+	 $(".pc3").click(function(){
+	 	clicked3 = !clicked3;
+	 	//console.log(clicked);
+	 	if(clicked3) {
+	 	$(".infoPc3").slideDown(1000); 
+	 } else {
+	 	$(".infoPc3").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc4").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc4").slideDown(1000); 
+	 } else {
+	 	$(".infoPc4").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc5").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc5").slideDown(1000); 
+	 } else {
+	 	$(".infoPc5").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc6").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc6").slideDown(1000); 
+	 } else {
+	 	$(".infoPc6").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc7").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc7").slideDown(1000); 
+	 } else {
+	 	$(".infoPc7").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc8").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc8").slideDown(1000); 
+	 } else {
+	 	$(".infoPc8").slideUp(1000);
+	 }
+	 })
+})
+
+$(document).ready(function() {
+	let clicked = false;
+	 $(".pc9").click(function(){
+	 	clicked = !clicked;
+	 	//console.log(clicked);
+	 	if(clicked) {
+	 	$(".infoPc9").slideDown(1000); 
+	 } else {
+	 	$(".infoPc9").slideUp(1000);
+	 }
+	 })
+})
